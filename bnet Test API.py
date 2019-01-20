cf9e96c1509cdea3dc01bf0006619c2322e4597a
MLT <kimjust6@gmail.com>
2019-01-19 19:01:19 -0500

    added parameters to helper functions

--- bnet Test API.py
+++ bnet Test API.py
@@ -2,42 +2,51 @@ import requests
 import json
 import sys
 
-#function that calls the metadata api
-def metadata():
+'''function that calls the metadata api
+'''
+def metadata( region_id, realm_id, profile_id ):
 
     return
 
-#function that calls the profile api
-def profile():
+'''function that calls the profile api
+'''
+def profile( region, region_id, realm_id, profile_id, locale ):
 
     return
 
-#function that calls the ladder_summary api
-def ladder_summary():
+'''function that calls the ladder_summary api
+'''
+def ladder_summary( region, region_id, realm_id, profile_id, locale ):
 
     return
 
-#function that calls the ladder api
-def ladder():
+'''function that calls the ladder api
+'''
+def ladder( region, region_id, realm_id, profile_id, ladder_id, locale ):
 
     return
 
-#function that calls the grandmaster_leaderboard api
-def grandmaster_leaderboard():
+'''function that calls the grandmaster_leaderboard api
+'''
+def grandmaster_leaderboard( region, region_id ):
 
     return
 
-#function that calls the season api
-def season():
+'''function that calls the season api
+'''
+def season( region, region_id ):
 
     return
 
-#function that calls the player api
-def player():
+'''function that calls the player api
+'''
+def player( region, account_id ):
 
     return
 
-def get_token(blizzard_client_id, blizzard_client_secret):
+'''assigns token an access_token given a client_id and client_secret
+'''
+def get_token( blizzard_client_id, blizzard_client_secret, token ):
     #function that uses the client and returns the token
     parameters = { "grant_type" : "client_credentials",
                     "scope" : "sc2.profile",
@@ -45,22 +54,25 @@ def get_token(blizzard_client_id, blizzard_client_secret):
                     "client_secret" : blizzard_client_secret }
 
     response = requests.post("https://us.battle.net/oauth/token", data = parameters)
-    #print(response.json())
-    return response.json()["access_token"]
-    #return response.json()
+
+    token = response.json()["access_token"]
+    return
+
 
 
 def main():
     #declarations and definitions
     base_url = 'https://us.api.blizzard.com'
     player_addon = "/sc2/player/"
-    blizzard_client_id = ""
+	blizzard_client_id = ""
     blizzard_client_secret = ""
+    token = ""
+    user_input = ""
 
     #requests blizzard api for a token
-    token = get_token(blizzard_client_id, blizzard_client_secret)
-    #token = "UScF6hzrVPN11o8HYguFq2bE2r2J4lYNjk"
-    user_input = ""
+    #the value of token is overwritten
+    get_token( blizzard_client_id, blizzard_client_secret, token )
+
 
     #get user input to see what they want to do
     #here is where we would  get authorization if our token expires
@@ -84,7 +96,7 @@ if __name__ == '__main__':
 
 
 
-accountId = "1"
+account_id = "1"
 
 
 
