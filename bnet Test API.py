a598063bacc7cf3a13dd9d403965c1ca4558b1e4
MLT <kimjust6@gmail.com>
2019-01-20 12:01:08 -0500

    added .gitignore file and moved the client and secret out of the main python file

--- bnet Test API.py
+++ bnet Test API.py
@@ -5,47 +5,63 @@ import sys
 '''function that calls the metadata api
 '''
 def metadata( region_id, realm_id, profile_id ):
-    parameters = {"region" : "US",
-                  "regionId" : region_id,
-                  "realmId" : realm_id,
-                  "profileId" : profile_id,
-                  "locale" : "en_US"}
+    parameters = { "region" : "US",
+                   "regionId" : region_id,
+                   "realmId" : realm_id,
+                   "profileId" : profile_id,
+                   "locale" : "en_US" }
     return
 
 '''function that calls the profile api
 '''
-def profile( region, region_id, realm_id, profile_id ):
-
+def profile( region_id, realm_id, profile_id ):
+    parameters = { "region" : "US",
+                   "regionId" : region_id,
+                   "realmId" : realm_id,
+                   "profileId" : profile_id,
+                   "locale" : "en_US" }
     return
 
 '''function that calls the ladder_summary api
 '''
-def ladder_summary( region, region_id, realm_id, profile_id ):
-
+def ladder_summary( region_id, realm_id, profile_id ):
+    parameters = { "region" : "US",
+                   "regionId" : region_id,
+                   "realmId" : realm_id,
+                   "profileId" : profile_id,
+                   "locale" : "en_US" }
     return
 
 '''function that calls the ladder api
 '''
-def ladder( region, region_id, realm_id, profile_id, ladder_id ):
-
+def ladder( region_id, realm_id, profile_id, ladder_id ):
+    parameters = { "region" : "US",
+                   "regionId" : region_id,
+                   "realmId" : realm_id,
+                   "profileId" : profile_id,
+                   "ladderId" : ladder_id,
+                   "locale" : "en_US" }
     return
 
 '''function that calls the grandmaster_leaderboard api
 '''
 def grandmaster_leaderboard( region, region_id ):
-
+    parameters = { "region" : "US",
+                   "regionId" : region_id }
     return
 
 '''function that calls the season api
 '''
-def season( region, region_id ):
-
+def season( region_id ):
+    parameters = { "region" : "US",
+                   "regionId" : region_id }
     return
 
 '''function that calls the player api
 '''
 def player( region, account_id ):
-
+    parameters = { "region" : "US",
+                   "accountId" : account_id }
     return
 
 '''assigns token an access_token given a client_id and client_secret
@@ -59,7 +75,7 @@ def get_token( blizzard_client_id, blizzard_client_secret ):
 
     response = requests.post("https://us.battle.net/oauth/token", data = parameters)
 
-    #token = response.json()["access_token"]
+    print( response.json() )
     return response.json()["access_token"]
 
 
@@ -68,14 +84,14 @@ def main():
     #declarations and definitions
     base_url = 'https://us.api.blizzard.com'
     player_addon = "/sc2/player/"
-    token = ""
-    user_input = ""
-    blizzard_client_id = "3f6567f1172c4c56bb36388cea125773"
-    blizzard_client_secret = "b7MgOxm5Hn4l47s2nHyK8JcuMLtqCIS2"
+    token = user_input = ""
+    import secret_and_key
+    #from "secret_and_key" import *
+    print( secret_and_key.blizzard_client_id, secret_and_key.blizzard_client_secret )
 
     #requests blizzard api for a token
     #the value of token is overwritten
-    token = get_token( blizzard_client_id, blizzard_client_secret )
+    token = get_token( secret_and_key.blizzard_client_id, secret_and_key.blizzard_client_secret )
 
 
     #get user input to see what they want to do
