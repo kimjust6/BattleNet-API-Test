33585b854ee536021a589d81ed61df8e7cc2d677
MLT <kimjust6@gmail.com>
2019-01-21 20:47:24 -0500

    wrote the bodies of the helper functions but did not test

--- bnet Test API.py
+++ bnet Test API.py
@@ -13,61 +13,113 @@ def metadata( base_url, region_id, realm_id, profile_id ):
                    "regionId" : region_id,
                    "realmId" : realm_id,
                    "profileId" : profile_id,
-                   "locale" : "en_US" }
+                   "locale" : "en_US",
+                   "access_token" : token }
 
-    addon_url = "/sc2/metadata/profile/"
+    api_path = "/sc2/metadata/profile/"
+               + region_id + "/"
+               + realm_id + "/"
+               + profile_id
     #make the api call
-    return requests.get( base_url + addon_url, params = parameters )
+    url = base_url + api_path
+    response = requests.get( url, params = parameters )
+    return response
 
 
 '''function that calls the profile api
 '''
-def profile( region_id, realm_id, profile_id ):
+def profile( base_url, token, region_id, realm_id, profile_id ):
     #set the parameters
     parameters = { "region" : "US",
                    "regionId" : region_id,
                    "realmId" : realm_id,
                    "profileId" : profile_id,
-                   "locale" : "en_US" }
-    addon_url = " /sc2/profile/"
+                   "locale" : "en_US",
+                   "access_token" : token }
+
+    api_path = "/sc2/profile/"
+               + region_id + "/"
+               + realm_id + "/"
+               + profile_id
+
+    url = base_url + api_path
     #make the api call
-    response = requests.get( base_url + addon_url, params = parameters )
-    return
+    response = requests.get( url, params = parameters )
+
+    return response
 
 '''function that calls the ladder_summary api
 '''
-def ladderSummary( region_id, realm_id, profile_id ):
+def ladderSummary( base_url, token, region_id, realm_id, profile_id ):
     parameters = { "region" : "US",
                    "regionId" : region_id,
                    "realmId" : realm_id,
                    "profileId" : profile_id,
-                   "locale" : "en_US" }
-    return
+                   "locale" : "en_US",
+                   "access_token" : token }
+
+    api_path = "/sc2/profile/"
+               + region_id + "/"
+               + realm_id + "/"
+               + profile_id + "/"
+               + "ladder"  + "/"
+               + "summary"
+
+    url = base_url + api_path
+    #make the api call
+    response = requests.get( url, params = parameters )
+
+    return response
 
 '''function that calls the ladder api
 '''
-def ladder( region_id, realm_id, profile_id, ladder_id ):
+def ladder( base_url, token, region_id, realm_id, profile_id, ladder_id ):
     parameters = { "region" : "US",
                    "regionId" : region_id,
                    "realmId" : realm_id,
                    "profileId" : profile_id,
                    "ladderId" : ladder_id,
-                   "locale" : "en_US" }
-    return
+                   "locale" : "en_US",
+                   "access_token" : token }
+
+    api_path = "/sc2/profile/"
+               + region_id + "/"
+               + realm_id + "/"
+               + profile_id + "/"
+               + "ladder" + "/"
+               + ladder_id
+    url = base_url + api_path
+    #make the api call
+    response = requests.get( url, params = parameters )
+
+    return response
 
 '''function that calls the grandmaster_leaderboard api
 '''
-def grandmasterLeaderboard( region, region_id ):
+def grandmasterLeaderboard( base_url, token, region_id ):
     parameters = { "region" : "US",
-                   "regionId" : region_id }
-    return
+                   "regionId" : region_id,
+                   "access_token" : token }
+    api_path = "/sc2/ladder/grandmaster/"
+               + region_id
+    url = base_url + api_path
+    #make the api call
+    response = requests.get( url, params = parameters )
+    return response
 
 '''function that calls the season api
 '''
-def season( region_id ):
+def season( base_url, token, region_id ):
     parameters = { "region" : "US",
-                   "regionId" : region_id }
-    return
+                   "regionId" : region_id,
+                   "access_token" : token }
+
+    api_path = "/sc2/ladder/season"
+               + region_id
+    url = base_url + api_path
+    #make the api call
+    response = requests.get( url, params = parameters )
+    return response
 
 '''function that calls the player api
 '''
@@ -76,11 +128,11 @@ def player( base_url, token, account_id ):
     parameters = { "region" : "US",
                    "accountId" : account_id,
                    "access_token" : token }
-    addon_url = "/sc2/player/"
-    url = base_url + addon_url
+    api_path = "/sc2/player/"
+    url = base_url + api_path
     response = requests.get( url + account_id, params = parameters )
-    print( url )
-    print( token )
+    #print( url )
+    #print( token )
     return response
 
 '''function that uses the client_id and client_secret and returns the token
@@ -98,12 +150,16 @@ def getToken( blizzard_client_id, blizzard_client_secret ):
     #print( response.json() )
     return response.json()["access_token"]
 
-
+'''function that checks if the response from a requests is valid'''
+def isResponseValid( response ):
+    if response.status_code == 200 and len( response.json() ) > 0:
+        return True
+    else:
+        return False
 
 def main():
     #declarations and definitions
     base_url = 'https://us.api.blizzard.com'
-    player_addon = "/sc2/player/"
     token = user_input = ""
 
 
@@ -114,18 +170,46 @@ def main():
 
     #get user input to see what they want to do
     #here is where we would  get authorization if our token expires
-    user_input = input("Please enter an account number:\n")
+    user_input = input("Please choose from the following options:\
+                       \n1) Metadata\
+                       \n2) Profile\
+                       \n3) Ladder Summary\
+                       \n4) Grandmaster Leaderboard\
+                       \n5) Season\
+                       \n6) Player\n")
+
+    #keep looping until user chooses to exit
     while user_input != "exit":
-        response = player( base_url, token, user_input )
+        if user_input == "1":
+            print("1")
+        elif user_input == "2":
+            print("2")
+        elif user_input == "3":
+            print("3")
+        elif user_input == "4":
+            print("4")
+        elif user_input == "5":
+            print("5")
+        elif user_input == "6":
+            user_input = input("Please enter an accountId:\n")
+            response = player( base_url, token, user_input )
 
-        #check if response is valid
-        if response.status_code == 200 and len( response.json() ) > 0:
-            print("The account belongs to: " + response.json()[0]["name"])
+            #check if response is valid
+            if isResponseValid( response ) == True:
+                print("The account belongs to: " + response.json()[0]["name"] + "\n")
+            else:
+                print( "Error! Code: " + str(response.status_code) + "\n" )
+
         else:
-            print( "Error! Code: " + str(response.status_code) )
+            print("Invalid input.\n\n")
 
-        user_input = input("Please enter an account number:\n")
-
+        user_input = input("Please choose from the following options:\
+                           \n1) Metadata\
+                           \n2) Profile\
+                           \n3) Ladder Summary\
+                           \n4) Grandmaster Leaderboard\
+                           \n5) Season\
+                           \n6) Player\n")
     #end program
     sys.exit()
 
