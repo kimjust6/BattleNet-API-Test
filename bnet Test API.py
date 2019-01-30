8a5018f79689e7b711f0f50b217473090f639e79
MLT <kimjust6@gmail.com>
2019-01-29 21:48:34 -0500

    added functionality to all the api calls

--- bnet Test API.py
+++ bnet Test API.py
@@ -16,6 +16,14 @@ class blizz_api:
         #print( response )
         pass
 
+#checks if repsonse
+def printResponse( response ):
+        if isResponseValid( response ) == True:
+            #print("The account belongs to: " + response.json()[0]["name"] + "\n")
+            print( str(response.json()) + "\n" )
+        else:
+            print( "Error! Code: " + str(response.status_code) + "\n" )
+        return
 
 '''function that calls the metadata api
 '''
@@ -115,6 +123,8 @@ def grandmasterLeaderboard( base_url, token, region_id ):
     api_path = "/sc2/ladder/grandmaster/" \
                + region_id
     url = base_url + api_path
+    #print( url )
+    #print( token )
     #make the api call
     response = requests.get( url, params = parameters )
     return response
@@ -126,7 +136,7 @@ def season( base_url, token, region_id ):
                    "regionId" : region_id,
                    "access_token" : token }
 
-    api_path = "/sc2/ladder/season" \
+    api_path = "/sc2/ladder/season/" \
                + region_id
     url = base_url + api_path
     #make the api call
@@ -188,30 +198,38 @@ def main():
                        \n3) Ladder Summary\
                        \n4) Grandmaster Leaderboard\
                        \n5) Season\
-                       \n6) Player\n")
+                       \n6) Player\
+                       \n7) Exit\n")
 
     #keep looping until user chooses to exit
-    while user_input != "exit":
+    while user_input != "exit" and user_input != "7":
         if user_input == "1":
-            print("1")
+            region_id = input( "Please enter server:\n" )
+            profile_id = input( "Please enter profile_id:\n" )
+            response = metadata( base_url, token, region_id, "1", profile_id )
         elif user_input == "2":
-            print("2")
+            region_id = input( "Please enter a server:\n" )
+            profile_id = input( "Please enter profile_id:\n" )
+            response = profile( base_url, token, region_id, "1", profile_id )
+            printResponse( response )
         elif user_input == "3":
-            print("3")
+            region_id = input( "Please enter a server:\n" )
+            profile_id = input( "Please enter profile_id:\n" )
+            response = ladderSummary( base_url, token, region_id, "1", profile_id )
+            printResponse( response )
         elif user_input == "4":
-            print("4")
+            user_input = input( "Please enter a server:\n" )
+            response = grandmasterLeaderboard( base_url, token, user_input )
+            printResponse( response )
         elif user_input == "5":
-            print("5")
+            user_input = input( "Please enter a server:\n" )
+            response = season( base_url, token, user_input )
+            printResponse( response )
         elif user_input == "6":
-            user_input = input("Please enter an accountId:\n")
+            user_input = input( "Please enter an accountId:\n" )
             response = player( base_url, token, user_input )
-
-            #check if response is valid
-            if isResponseValid( response ) == True:
-                print("The account belongs to: " + response.json()[0]["name"] + "\n")
-            else:
-                print( "Error! Code: " + str(response.status_code) + "\n" )
-
+            #printResponse already checks if response is valid, do not worry
+            printResponse( response )
         else:
             print("Invalid input.\n\n")
 
@@ -221,7 +239,8 @@ def main():
                            \n3) Ladder Summary\
                            \n4) Grandmaster Leaderboard\
                            \n5) Season\
-                           \n6) Player\n")
+                           \n6) Player\
+                           \n7) Exit\n")
     #end program
     sys.exit()
 
